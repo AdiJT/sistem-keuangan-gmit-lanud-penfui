eedb0eed49d1acf8b232b4a5c7ba66df1496c188
AdiJT <aditaklal@gmail.com>
2024-11-13 08:31:02 +0800

    Buat action tambah kelompok akun di controller akun

--- src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Web/Areas/Dashboard/Controllers/AkunController.cs
+++ src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Web/Areas/Dashboard/Controllers/AkunController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SIKeuanganGMITLanudPenfui.Application.AkunCQ.Commands.CreateJenisAkun;
+using SIKeuanganGMITLanudPenfui.Application.AkunCQ.Commands.CreateKelompokAkun;
 using SIKeuanganGMITLanudPenfui.Domain.Entities;
 using SIKeuanganGMITLanudPenfui.Domain.Enums;
 using SIKeuanganGMITLanudPenfui.Domain.Repositories;
@@ -19,9 +20,6 @@ public class AkunController : Controller
     private readonly IRepositoriKelompokAkun _repositoriKelompokAkun;
     private readonly IRepositoriGolonganAkun _repositoriGolonganAkun;
     private readonly ISender _sender;
-
-    private const string TambahJenisAkunViewName = "TambahJenisAkun";
-    private const string TambahKelompokAkunViewName = "TambahKelompokAkun";
 
     public AkunController(
         IRepositoriAkun repositoriAkun,
@@ -85,33 +83,29 @@ public class AkunController : Controller
         });
     }
 
-    [Route("[area]/[controller]/Penerimaan/{tahun:int}/TambahJenisAkun")]
-    public IActionResult TambahJenisAkunPenerimaan(int tahun) => 
-        View(
-            TambahJenisAkunViewName, 
-            new TambahJenisAkunVM 
-            {
-                Tahun = tahun,
-                Jenis = Jenis.Penerimaan,
-                ReturnUrl = Url.Action(nameof(TambahJenisAkunPenerimaan), "Akun", new { Area = "Dashboard", tahun })!
-            }
-        );
+    [Route("[area]/[controller]/{jenis}/{tahun:int}/TambahJenisAkun")]
+    public IActionResult TambahJenisAkun(Jenis jenis, int tahun)
+    {
+        if (Tahun.Create(tahun).IsFailure)
+            return BadRequest();
 
-    [Route("[area]/[controller]/Belanja/{tahun:int}/TambahJenisAkun")]
-    public IActionResult TambahJenisAkunBelanja(int tahun) =>
-        View(
-            TambahJenisAkunViewName,
+        return View(
             new TambahJenisAkunVM
             {
                 Tahun = tahun,
-                Jenis = Jenis.Belanja,
-                ReturnUrl = Url.Action(nameof(TambahJenisAkunBelanja), "Akun", new { Area = "Dashboard", tahun })!
+                Jenis = Jenis.Penerimaan,
+                ReturnUrl = Url.Action(jenis == Jenis.Penerimaan ? nameof(Penerimaan) : nameof(Belanja), "Akun", new { Area = "Dashboard", tahun })!
             }
         );
+    }
 
     [HttpPost]
-    public async Task<IActionResult> TambahJenisAkun(TambahJenisAkunVM tambahJenisAkunVM)
+    [Route("[area]/[controller]/{jenis}/{tahun:int}/TambahJenisAkun")]
+    public async Task<IActionResult> TambahJenisAkun([FromForm]TambahJenisAkunVM tambahJenisAkunVM, [FromRoute]Jenis jenis, [FromRoute]int tahun)
     {
+        if(tambahJenisAkunVM.Jenis != jenis || tambahJenisAkunVM.Tahun != tahun)
+            return BadRequest();
+
         var createJenisAkunCommand = new CreateJenisAkunCommand(tambahJenisAkunVM.Uraian, tambahJenisAkunVM.Tahun, tambahJenisAkunVM.Jenis);
         var result = await _sender.Send(createJenisAkunCommand);
 
@@ -122,5 +116,37 @@ public class AkunController : Controller
         }
 
         return Redirect(tambahJenisAkunVM.ReturnUrl);
+    }
+
+    [Route("[area]/[controller]/{jenis}/{tahun:int}/TambahKelompokAkun")]
+    public IActionResult TambahKelompokAkun(Jenis jenis, int tahun)
+    {
+        if (Tahun.Create(tahun).IsFailure)
+            return BadRequest();
+
+        return View(new TambahKelompokAkunVM(_repositoriJenisAkun)
+        {
+            Jenis = jenis,
+            Tahun = tahun,
+            ReturnURL = Url.Action(jenis == Jenis.Penerimaan ? nameof(Penerimaan) : nameof(Belanja), "Akun", new { Area = "Dashboard", tahun, jenis})!
+        });
+    }
+
+    [HttpPost]
+    [Route("[area]/[controller]/{jenis}/{tahun:int}/TambahKelompokAkun")]
+    public async Task<IActionResult> TambahKelompokAkun([FromForm] TambahKelompokAkunVM vm, [FromRoute] Jenis jenis, [FromRoute] int tahun)
+    {
+        if (vm.Tahun != tahun || vm.Jenis != jenis)
+            return BadRequest();
+
+        var command = new CreateKelompokAkunCommand(vm.Uraian, vm.Tahun, vm.IdJenisAkun);
+        var result = await _sender.Send(command);
+        if(result.IsFailure)
+        {
+            ModelState.AddModelError(string.Empty, $"Error telah terjadi. Code = {result.Error.Code}, Message = {result.Error.Message}");
+            return View(vm);
+        }
+
+        return Redirect(vm.ReturnURL);
     }
 }
--- src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Web/Areas/Dashboard/Models/AkunModels/TambahKelompokAkunVM.cs
+++ src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Web/Areas/Dashboard/Models/AkunModels/TambahKelompokAkunVM.cs
@@ -1,5 +1,40 @@
-﻿namespace SIKeuanganGMITLanudPenfui.Web.Areas.Dashboard.Models.AkunModels;
+﻿using SIKeuanganGMITLanudPenfui.Domain.Entities;
+using SIKeuanganGMITLanudPenfui.Domain.Enums;
+using SIKeuanganGMITLanudPenfui.Domain.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace SIKeuanganGMITLanudPenfui.Web.Areas.Dashboard.Models.AkunModels;
 
 public class TambahKelompokAkunVM
 {
+    private readonly IRepositoriJenisAkun _repositoriJenisAkun;
+
+    [Display(Name = "Uraian")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public string Uraian { get; set; } = string.Empty;
+
+    [Display(Name = "Jenis")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public required Jenis Jenis { get; set; }
+
+    [Display(Name = "Tahun")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public required int Tahun { get; set; }
+
+    [Display(Name = "Jenis Akun")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public int IdJenisAkun { get; set; }
+
+    [Required]
+    public required string ReturnURL { get; set; }
+
+    public async Task<List<JenisAkun>> DaftarJenisAkun() => 
+        (await _repositoriJenisAkun.GetAll())
+        .Where(j => j.Tahun.Value == Tahun && j.Jenis == Jenis)
+        .ToList();
+
+    public TambahKelompokAkunVM(IRepositoriJenisAkun repositoriJenisAkun)
+    {
+        _repositoriJenisAkun = repositoriJenisAkun;
+    }
 }
