df9a94675255cd46b3ceb1b745d5959fa569f665
AdiJT <aditaklal@gmail.com>
2024-11-22 16:08:52 +0800

    Sedikit

---
+++ src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Infrastructure/Services/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using SIKeuanganGMITLanudPenfui.Domain.Shared;
+
+namespace SIKeuanganGMITLanudPenfui.Infrastructure.Services;
+
+public interface IFileService
+{
+    bool IsFileExist(string folderPath, string fileName);
+    Uri GetFileUri(string folderPath, string fileName);
+    Task<Result<string>> UploadFile<TModel>(IFormFile formFile, string folderPath, string[] permittedExtension, long minSizeLimit, long maxSizeLimit);
+}
