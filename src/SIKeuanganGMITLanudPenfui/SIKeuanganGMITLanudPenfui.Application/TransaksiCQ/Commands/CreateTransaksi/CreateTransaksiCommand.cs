0e69f1610d5ce41ec492683f26bbc398988acfbd
AdiJT <aditaklal@gmail.com>
2024-11-25 18:03:48 +0800

    Gabung belanja dan penerimaan jadi transaksi

---
+++ src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Application/TransaksiCQ/Commands/CreateTransaksi/CreateTransaksiCommand.cs
@@ -0,0 +1,13 @@
+﻿using SIKeuanganGMITLanudPenfui.Application.Abstracts;
+using SIKeuanganGMITLanudPenfui.Domain.Enums;
+
+namespace SIKeuanganGMITLanudPenfui.Application.TransaksiCQ.Commands.CreateTransaksi;
+
+public record CreateTransaksiCommand(
+    DateOnly Tanggal,
+    string Uraian,
+    string Jumlah,
+    Uri FileBukti,
+    Jenis Jenis,
+    int AkunId,
+    int KasId) : ICommand { }
