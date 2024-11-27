e1fb150d0a38bbdcceaa1715706b3122b6913b31
AdiJT <aditaklal@gmail.com>
2024-11-27 12:08:25 +0800

    Selesai laporan

---
+++ src/SIKeuanganGMITLanudPenfui/SIKeuanganGMITLanudPenfui.Web/ViewComponents/ChartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIKeuanganGMITLanudPenfui.Web.Models;
+
+namespace SIKeuanganGMITLanudPenfui.Web.ViewComponents;
+
+public class ChartViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(ChartVM chartModel)
+    {
+        return View(chartModel);
+    }
+}
